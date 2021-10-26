d32e1871e2855bdc879831b0e5b9ca0f7c4d8770
nguye374 <nguye374@sheridancollege.ca>
2021-10-26 19:33:50 -0400

    Oct 26

---
+++ build/precache-manifest.c2f1a524e848d39617ac3f877d12f7bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14a7d9da1df8a0b2b06cae1cca00e581",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a911e0d4070952131e13",
+    "url": "/static/css/main.29750fd1.chunk.css"
+  },
+  {
+    "revision": "a6f4be9303cc72106aca",
+    "url": "/static/js/2.fa282abe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.fa282abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a911e0d4070952131e13",
+    "url": "/static/js/main.dafe8dbb.chunk.js"
+  },
+  {
+    "revision": "f13d5fb20258b992088c",
+    "url": "/static/js/runtime-main.a6b2f021.js"
+  }
+]);
